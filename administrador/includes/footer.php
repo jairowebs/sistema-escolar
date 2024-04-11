082a699e4e518dc741463fcd024e007dc425338c
jairowebs <136769364+jairowebs@users.noreply.github.com>
2024-04-11 11:46:01 -0500

    .

--- administrador/includes/footer.php
+++ administrador/includes/footer.php
@@ -1,7 +1,7 @@
  <!-- Essential javascripts for application to work-->
-    <script src="js/jquery-3.7.0.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/main.js"></script>
+    <script src="../js/jquery-3.7.0.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/main.js"></script>
     <!-- Page specific javascripts-->
     <!-- Google analytics script-->
     
