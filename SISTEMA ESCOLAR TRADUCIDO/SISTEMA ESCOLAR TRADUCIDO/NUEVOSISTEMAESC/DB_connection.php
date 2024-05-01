8c24a6dae1fde85da3862c4fe311fadad0a51c74
jairowebs <136769364+jairowebs@users.noreply.github.com>
2024-05-01 18:53:32 -0500

    .

--- SISTEMA ESCOLAR TRADUCIDO/SISTEMA ESCOLAR TRADUCIDO/NUEVOSISTEMAESC/DB_connection.php
+++ SISTEMA ESCOLAR TRADUCIDO/SISTEMA ESCOLAR TRADUCIDO/NUEVOSISTEMAESC/DB_connection.php
@@ -1,14 +1,14 @@
-<?php  
+<?php
 
 $sName = "localhost:3307";
 $uName = "root";
-$pass  = "";
+$pass = "";
 $db_name = "smsdb";
 
 try {
 	$conn = new PDO("mysql:host=$sName;dbname=$db_name", $uName, $pass);
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}catch(PDOExeption $e){
-	echo "Connection failed: ". $e->getMessage();
+} catch (PDOExeption $e) {
+	echo "Connection failed: " . $e->getMessage();
 	exit;
 }
