5e1e835e5734da9eea3e4dd6ac191c62c2c29fa5
jairowebs <136769364+jairowebs@users.noreply.github.com>
2024-05-01 19:15:12 -0500

    m

---
+++ admin/student-view.php
@@ -0,0 +1,108 @@
+<?php 
+session_start();
+if (isset($_SESSION['admin_id']) && 
+    isset($_SESSION['role'])) {
+
+    if ($_SESSION['role'] == 'Admin') {
+       include "../DB_connection.php";
+       include "data/student.php";
+       include "data/subject.php";
+       include "data/grade.php";
+       include "data/section.php";
+
+       if(isset($_GET['student_id'])){
+
+       $student_id = $_GET['student_id'];
+
+       $student = getStudentById($student_id, $conn);    
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Estudiantes - Profesores</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="../css/style.css">
+	<link rel="icon" href="../logo.png">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <?php 
+        include "inc/navbar.php";
+        if ($student != 0) {
+     ?>
+     <div class="container mt-5">
+         <div class="card" style="width: 22rem;">
+          <img src="../img/student-<?=$student['gender']?>.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title text-center">@<?=$student['username']?></h5>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Nombre: <?=$student['fname']?></li>
+            <li class="list-group-item">Apellido: <?=$student['lname']?></li>
+            <li class="list-group-item">Nombre Usuario: <?=$student['username']?></li>
+            <li class="list-group-item">Direccion: <?=$student['address']?></li>
+            <li class="list-group-item">Fecha de Nacimiento: <?=$student['date_of_birth']?></li>
+            <li class="list-group-item">Correo Electronico: <?=$student['email_address']?></li>
+            <li class="list-group-item">Genero: <?=$student['gender']?></li>
+            <li class="list-group-item">Fecha de Ingreso: <?=$student['date_of_joined']?></li>
+
+            <li class="list-group-item">Grado: 
+                 <?php 
+                      $grade = $student['grade'];
+                      $g = getGradeById($grade, $conn);
+                      echo $g['grade_code'].'-'.$g['grade'];
+                  ?>
+            </li>
+            <li class="list-group-item">Seccion: 
+                 <?php 
+                    $section = $student['section'];
+                    $s = getSectioById($section, $conn);
+                    echo $s['section'];
+                  ?>
+            </li>
+            <br><br>
+            <li class="list-group-item">Nombre acudiente: <?=$student['parent_fname']?></li>
+            <li class="list-group-item">Apellido Acudiente: <?=$student['parent_lname']?></li>
+            <li class="list-group-item">Numero contacto acudiente: <?=$student['parent_phone_number']?></li>
+          </ul>
+          <div class="card-body">
+            <a href="student.php" class="card-link">Regresar</a>
+          </div>
+        </div>
+     </div>
+     <?php 
+        }else {
+          header("Location: teacher.php");
+          exit;
+        }
+     ?>
+     
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>	
+    <script>
+        $(document).ready(function(){
+             $("#navLinks li:nth-child(3) a").addClass('active');
+        });
+    </script>
+
+</body>
+</html>
+<?php 
+
+    }else {
+        header("Location: student.php");
+        exit;
+    }
+
+  }else {
+    header("Location: ../login.php");
+    exit;
+  } 
+}else {
+	header("Location: ../login.php");
+	exit;
+} 
+
+?>
